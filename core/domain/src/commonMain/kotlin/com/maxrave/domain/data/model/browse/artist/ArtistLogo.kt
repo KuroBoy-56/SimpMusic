6d0b04ee064a45ac3bc8451de34a56608c865673
KuroBoy <213059450+KuroBoy-56@users.noreply.github.com>
2026-08-02 11:13:52 -0500

    Add ArtistLogo data class with serialization

--- core/domain/src/commonMain/kotlin/com/maxrave/domain/data/model/browse/artist/ArtistLogo.kt
+++ core/domain/src/commonMain/kotlin/com/maxrave/domain/data/model/browse/artist/ArtistLogo.kt
@@ -1,1 +1,11 @@
+package com.maxrave.domain.data.model.browse.artist
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ArtistLogo(
+    val logoUrl: String,
+    val bgColorHex: String?,
+    val width: Int,
+    val height: Int,
+)
