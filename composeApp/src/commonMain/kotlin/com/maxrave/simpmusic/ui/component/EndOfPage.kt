f39348fda875de4b658377da02d43a51c75914ee
KuroBoy-56 <213059450+KuroBoy-56@users.noreply.github.com>
2026-09-13 21:04:00 -0500

    Fix: Hash de producción definitivo, rutas de API encriptadas y seguridad anticrack 100% activa

--- composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/EndOfPage.kt
+++ composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/EndOfPage.kt
@@ -34,7 +34,7 @@ fun EndOfPage(withoutCredit: Boolean = false) {
                     stringResource(
                         Res.string.version_format,
                         VersionManager.getVersionName(),
-                    ) + "\ngarevyn-dev",
+                    ) + "\nGarevyn-dev",
                 style = typo().bodySmall,
                 textAlign = TextAlign.Center,
                 modifier =
